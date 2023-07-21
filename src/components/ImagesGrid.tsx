c7dac5b4c4e632e4de36ed9eb33c2ae879fe4ca9
Lucas Klein <lucas.fklein@hotmail.com>
2023-07-21 19:58:11 +0700

    masonry layout

--- src/components/ImagesGrid.tsx
+++ src/components/ImagesGrid.tsx
@@ -1,32 +1,27 @@
-import { Box, SimpleGrid } from "@mantine/core";
+import { Box } from "@mantine/core";
 import Image from "next/image";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import { api } from "~/utils/api";
 
 const ImagesGrid = () => {
   const { data } = api.example.getImages.useQuery();
 
   return (
-    <SimpleGrid
-      spacing={4}
-      cols={4}
-      breakpoints={[
-        { maxWidth: "62rem", cols: 3, spacing: "md" },
-        { maxWidth: "48rem", cols: 2, spacing: "sm" },
-        { maxWidth: "36rem", cols: 1, spacing: "sm" },
-      ]}
-    >
-      {data?.map((image, i) => (
-        <Box key={i}>
-          <Image
-            src={image.imageUrl}
-            alt={`Image ${i}`}
-            width={120}
-            height={120}
-            layout="responsive"
-          />
-        </Box>
-      ))}
-    </SimpleGrid>
+    <ResponsiveMasonry columnsCountBreakPoints={{ 350: 2, 750: 3, 900: 4 }}>
+      <Masonry gutter="0.5rem">
+        {data?.map((image, i) => (
+          <Box key={i}>
+            <Image
+              src={image.imageUrl}
+              alt={`Image ${i}`}
+              width={120}
+              height={120}
+              layout="responsive"
+            />
+          </Box>
+        ))}
+      </Masonry>
+    </ResponsiveMasonry>
   );
 };
 
